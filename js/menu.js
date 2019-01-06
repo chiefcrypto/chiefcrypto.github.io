9c175108cd13c47ee25e4bc427920cdcdc2dc95e
chiefcrypto <chshellard@gmail.com>
2019-01-06 19:56:37 +0000

    renew

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".burger-nav").on("click", function() {
-    $(".mobile-menu").toggleClass("mobile-menu-active");
-    $("main").toggleClass("hidden");
-    $(".burger-nav").toggleClass("fa-times");
-  });
-});
